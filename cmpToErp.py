5e0c3f4a1382f8c8bb12f9b95b6b0f966ca7615c
Ibrahim DJEMAI <ibrahim_djemai@yahoo.fr>
2020-04-02 12:52:20 +0200

    First Commit

---
+++ cmpToErp.py
@@ -0,0 +1,155 @@
+import numpy as np
+
+
+def getErpPatch(cmpFaces):
+    '''
+    <h3>CubeMap Projection to EquiRectangular Projection</h3>
+    Inspired from https://github.com/rayryeng/cubic2equi/blob/master/cubic2equi.m
+    <b>Parameters:</b><ul>
+        <li><b>cmpFaces :</b> CubeMap Projected Faces (OpenCV BGR Format):
+        front, right, back, left, top, bottom
+        with shape : [6, H, W, 3]</li></ul>
+    <b>Returns:</b><ul>
+        <li><b>erpPatch :</b> Equirectangular Projected Image
+        with shape : [H, W*2, 3]</li></ul>
+    '''
+    height = cmpFaces.shape[1]
+    width = 2 * cmpFaces.shape[2]
+
+    faceXNeg = 4  # Front
+    faceZNeg = 1  # Right
+    faceXPos = 5  # Back
+    faceZPos = 0  # Left
+    faceYPos = 2  # Top
+    faceYNeg = 3  # Bottom
+
+    stackedImages = [faceXNeg, faceZNeg,
+                     faceXPos, faceZPos, faceYPos, faceYNeg]
+
+    facesB = np.ndarray(
+        (cmpFaces.shape[1], cmpFaces.shape[2], 6), dtype=cmpFaces.dtype)
+    facesG = np.copy(facesB)
+    facesR = np.copy(facesB)
+
+    for i, face in enumerate(cmpFaces):
+        facesB[:, :, stackedImages[i]] = face[:, :, 0]
+        facesG[:, :, stackedImages[i]] = face[:, :, 1]
+        facesR[:, :, stackedImages[i]] = face[:, :, 1]
+
+    X, Y = np.meshgrid(np.linspace(0, width, width, endpoint=False),
+                       np.linspace(0, height, height, endpoint=False))
+
+    Y = 2*Y/height - 1
+    X = 2*X/width - 1
+
+    sphereTheta = X*np.pi
+    spherePhi = Y*np.pi/2
+
+    texX = np.cos(spherePhi)*np.cos(sphereTheta)
+    texY = np.sin(spherePhi)
+    texZ = np.cos(spherePhi)*np.sin(sphereTheta)
+
+    comp = np.stack((texX, texY, texZ), axis=2)
+    ind = np.argmax(np.abs(comp), axis=2)
+    maxVal = np.zeros(shape=ind.shape)
+    maxVal[ind == 0] = texX[ind == 0]
+    maxVal[ind == 1] = texY[ind == 1]
+    maxVal[ind == 2] = texZ[ind == 2]
+
+    getFace = -1*np.ones(shape=maxVal.shape)
+
+    # Front
+    ind = np.logical_and(np.less(np.abs(maxVal - texX),
+                                 0.00001), np.greater_equal(texX, 0))
+    getFace[ind] = faceXNeg
+
+    # Right
+    ind = np.logical_and(np.less(np.abs(maxVal - texZ),
+                                 0.00001), np.greater_equal(texZ, 0))
+    getFace[ind] = faceZNeg
+
+    # Back
+    ind = np.logical_and(
+        np.less(np.abs(maxVal - texX), 0.00001), np.less(texX, 0))
+    getFace[ind] = faceXPos
+
+    # Left
+    ind = np.logical_and(
+        np.less(np.abs(maxVal - texZ), 0.00001), np.less(texZ, 0))
+    getFace[ind] = faceZPos
+
+    # Top
+    ind = np.logical_and(
+        np.less(np.abs(maxVal - texY), 0.00001), np.less(texY, 0))
+    getFace[ind] = faceYPos
+
+    # Bottom
+    ind = np.logical_and(np.less(np.abs(maxVal - texY),
+                                 0.00001), np.greater_equal(texY, 0))
+    getFace[ind] = faceYNeg
+
+    rawX = -1*np.ones(shape=maxVal.shape)
+    rawY = np.copy(rawX)
+    rawZ = np.copy(rawX)
+
+    # Front
+    ind = getFace == faceXNeg
+    rawX[ind] = texZ[ind]
+    rawY[ind] = texY[ind]
+    rawZ[ind] = texX[ind]
+
+    # Right
+    ind = getFace == faceZNeg
+    rawX[ind] = -texX[ind]
+    rawY[ind] = texY[ind]
+    rawZ[ind] = texZ[ind]
+
+    # Back
+    ind = getFace == faceXPos
+    rawX[ind] = -texZ[ind]
+    rawY[ind] = texY[ind]
+    rawZ[ind] = texX[ind]
+
+    # Left
+    ind = getFace == faceZPos
+    rawX[ind] = texX[ind]
+    rawY[ind] = texY[ind]
+    rawZ[ind] = texZ[ind]
+
+    # Top
+    ind = getFace == faceYPos
+    rawX[ind] = texZ[ind]
+    rawY[ind] = texX[ind]
+    rawZ[ind] = texY[ind]
+
+    # Bottom
+    ind = getFace == faceYNeg
+    rawX[ind] = texZ[ind]
+    rawY[ind] = -texX[ind]
+    rawZ[ind] = texY[ind]
+
+    rawCoords = np.stack((rawX, rawY, rawZ), axis=2)
+
+    cubeCoordsX = ((rawCoords[:, :, 0] / np.abs(rawCoords[:, :, 2])) + 1) / 2
+    cubeCoordsY = ((rawCoords[:, :, 1] / np.abs(rawCoords[:, :, 2])) + 1) / 2
+    cubeCoords = np.stack((cubeCoordsX, cubeCoordsY), axis=2)
+
+    normalizedX = np.around(cubeCoords[:, :, 0] * height)
+    normalizedY = np.around(cubeCoords[:, :, 1] * height)
+
+    normalizedX = np.clip(normalizedX, 0, height-1)
+    normalizedY = np.clip(normalizedY, 0, height-1)
+
+    normalizedCoords = np.stack(
+        (normalizedX, normalizedY), axis=2).astype(np.int32)
+
+    out = np.zeros(shape=(maxVal.shape[0], maxVal.shape[1], 3), dtype=np.uint8)
+
+    out[:, :, 0] = facesB[normalizedCoords[:, :, 1],
+                          normalizedCoords[:, :, 0], getFace.astype(np.int32)]
+    out[:, :, 1] = facesG[normalizedCoords[:, :, 1],
+                          normalizedCoords[:, :, 0], getFace.astype(np.int32)]
+    out[:, :, 2] = facesR[normalizedCoords[:, :, 1],
+                          normalizedCoords[:, :, 0], getFace.astype(np.int32)]
+
+    return out
---
+++ erpToCmp.py
@@ -0,0 +1,87 @@
+import numpy as np
+from scipy import ndimage
+
+
+def getCmpFaces(erpPatch, H=None, W=None):
+    '''
+    <h3>EquiRectangular Projection to CubeMap Projection Function</h3>
+    <b>Parameters:</b><ul>
+        <li><b>erpPatch :</b> Equirectangular Projected Image (OpenCV BGR Format)</li>
+        <li><b>H :</b> Height of CMP Faces (default : patch.shape[0] // 2)</li>
+        <li><b>W :</b> Width of CMP Faces (default : patch.shape[1] // 4)</b></li></ul>
+    <b>Returns:</b><ul>
+        <li><b>cmpFaces :</b> CubeMap Projected Faces:
+        front, right, back, left, top, bottom
+        with shape : [6, H, W, 3]</li></ul>
+    '''
+    V = [0, 0, 0, 0, -np.pi/2, np.pi/2]
+    U = [0, np.pi/2, np.pi, -np.pi/2, 0, 0]
+    fov = np.pi/2
+
+    if H is None:
+        H = erpPatch.shape[0] // 2
+    if W is None:
+        W = erpPatch.shape[1] // 4
+
+    cmpFaces = np.ndarray((6, H, W, 3))
+
+    for i, (u, v) in enumerate(zip(U, V)):
+        cmpFaces[i] = eqToPers(erpPatch, fov, u, v, H, W)
+
+    return cmpFaces
+
+
+def genXYZ(fov, u, v, outH, outW):
+    ''' Source : https://github.com/pepepor123/equirectangular-to-cubemap '''
+    out = np.ones((outH, outW, 3), np.float32)
+
+    xRng = np.linspace(-np.tan(fov / 2), np.tan(fov / 2),
+                       num=outW, dtype=np.float32)
+    yRng = np.linspace(-np.tan(fov / 2), np.tan(fov / 2),
+                       num=outH, dtype=np.float32)
+
+    out[:, :, :2] = np.stack(np.meshgrid(xRng, -yRng), -1)
+    Rx = np.array([[1, 0, 0], [0, np.cos(v), -np.sin(v)],
+                   [0, np.sin(v), np.cos(v)]])
+    Ry = np.array([[np.cos(u), 0, np.sin(u)], [
+                  0, 1, 0], [-np.sin(u), 0, np.cos(u)]])
+
+    R = np.dot(Ry, Rx)
+    return out.dot(R.T)
+
+
+def xyzToUV(xyz):
+    ''' Source : https://github.com/pepepor123/equirectangular-to-cubemap '''
+    x, y, z = np.split(xyz, 3, axis=-1)
+    u = np.arctan2(x, z)
+    c = np.sqrt(x ** 2 + z ** 2)
+    v = np.arctan2(y, c)
+    return np.concatenate([u, v], axis=-1)
+
+
+def uvToXY(uv, eqH, eqW):
+    ''' Source : https://github.com/pepepor123/equirectangular-to-cubemap '''
+    u, v = np.split(uv, 2, axis=-1)
+    X = (u / (2 * np.pi) + 0.5) * eqW - 0.5
+    Y = (-v / np.pi + 0.5) * eqH - 0.5
+    return np.concatenate([X, Y], axis=-1)
+
+
+def eqToPers(eqimg, fov, u, v, outH, outW):
+    ''' Source : https://github.com/pepepor123/equirectangular-to-cubemap '''
+    xyz = genXYZ(fov, u, v, outH, outW)
+    uv = xyzToUV(xyz)
+
+    eq_h, eq_w = eqimg.shape[:2]
+    XY = uvToXY(uv, eq_h, eq_w)
+
+    X, Y = np.split(XY, 2, axis=-1)
+    X = np.reshape(X, (outH, outW))
+    Y = np.reshape(Y, (outH, outW))
+
+    mc0 = ndimage.map_coordinates(eqimg[:, :, 0], [Y, X])  # channel: B
+    mc1 = ndimage.map_coordinates(eqimg[:, :, 1], [Y, X])  # channel: G
+    mc2 = ndimage.map_coordinates(eqimg[:, :, 2], [Y, X])  # channel: R
+
+    output = np.stack([mc0, mc1, mc2], axis=-1)
+    return output
